7a3bba742dbe0d969b0f18d16a88c8d55ffc46b1
Arj-design <giuseppe.roccolano.arj@gmail.com>
2026-07-02 11:05:40 +0200

    Update MainActivity.java

--- android/app/src/main/java/com/savethefridge/app/MainActivity.java
+++ android/app/src/main/java/com/savethefridge/app/MainActivity.java
@@ -1,18 +1,5 @@
 package com.savethefridge.app;
 
-import android.webkit.PermissionRequest;
-import android.webkit.WebChromeClient;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {
-    @Override
-    public void onStart() {
-        super.onStart();
-        this.bridge.getWebView().setWebChromeClient(new WebChromeClient() {
-            @Override
-            public void onPermissionRequest(final PermissionRequest request) {
-                request.grant(request.getResources());
-            }
-        });
-    }
-}
+public class MainActivity extends BridgeActivity {}
